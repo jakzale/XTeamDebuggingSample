865b82e295b33d3ec42acae4a29ebdecd1728f2f
Jakub Zalewski <jakzale@gmail.com>
2018-03-21 15:51:33 +0000

    Fix yeild to yield (lol)

--- src/FSharpLib/Library.fs
+++ src/FSharpLib/Library.fs
@@ -3,5 +3,5 @@ namespace FSharpLib
 [<AbstractClass; Sealed>]
 type Report private () =
     static member Report(science: int) =
-        sprintf "Our current science yeild is %i" science
+        sprintf "Our current science yield is %i" science
 
